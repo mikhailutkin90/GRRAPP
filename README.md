e6ef1a85c269eb3f5637329a5987241300ec8d50
screwdriver66 <simo.uimonen@gmail.com>
2023-09-09 22:35:52 +0300

    adds some structure, todo list, some small changes to folders, etc etc.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # TODO:
 
 - redo the structure according to pyproject toml from [here](https://geekflare.com/python-pyproject-toml/)
-- improve requirements 
+- add creation of virtualenvironment that can automatically be installed
+- improve requirements file (add all libraries, e.g. requests, numpy)
+- re-write by using functions, so that stuff doesn't get called for nothing
+- re-write tests with mocking of requests
+- create a cli too with click [here](https://click.palletsprojects.com/en/8.1.x/)
+- how to make a flask app [here](https://flask.palletsprojects.com/en/2.3.x/tutorial/)
+- how to containerize flask app in docker container [here](https://www.youtube.com/watch?v=FugtcQrcWYg)
+- how to create a web interface for my flask app in docker container with streamlit app [here](https://docs.streamlit.io/library/get-started/create-an-app)
+
+
+Extra:
+- add logging
+- add exceptions
+- test not 200 as well
+
+
+
--- src/actual.py
+++ src/actual.py
@@ -2,25 +2,38 @@ import requests
 import json
 import numpy as np
 
-
-api_key = 'cd4259afc066461f96e40d56cdc6fe76' # <-- remove this
-
-#For Porvoo
-lat = 60.3954
-lon = 25.6605
-
-#city weather now
-api_urlw = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
-response = requests.get(api_urlw)
-dataw = response.json()
-temp = dataw['main']['temp']-273
-wspeed = dataw['wind']['speed']
-hum = dataw['main']['humidity']
-
 #write the new line of actual weather
-newrowACT=np.array([temp, wspeed, hum])
-fromACT = np.loadtxt("ACTUAL.txt")
-sumACT=np.vstack((fromACT, newrowACT))
+
+def get_my_data(api_urlw):
+    '''docstring'''
+    response = requests.get(api_urlw)
+    dataw = response.json()
+    temp = dataw['main']['temp']-273
+    wspeed = dataw['wind']['speed']
+    hum = dataw['main']['humidity']
+    return temp, wspeed, hum
+
+def crunch_data(temp, wspeed, hum):
+    '''Return sumACT (wtf is sum_act)'''
+    newrow_act = np.array([temp, wspeed, hum])
+    from_act = np.loadtxt("ACTUAL.txt")
+    sum_act = np.vstack((from_act, newrow_act))
+    return sum_act
+
+
+if __name__ == "__main__":
+    api_key = 'cd4259afc066461f96e40d56cdc6fe76' # <-- change this to ENV var
+
+    #For Porvoo
+    lat = 60.3954
+    lon = 25.6605
+
+    #city weather now
+    api_urlw = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
+
+    temp, wspeed, hum = get_my_data(api_urlw)
+    sum_act = crunch_data(temp, wspeed, hum)
+    print("Sum act is: ", sum_act)
 
 
 '''city coordinates
